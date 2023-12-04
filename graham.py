f4ede277d09a311fa47932abfdbd34e01e8ce616
Jamie McIlroy <jmcilroy@gbefund.com>
2023-12-04 12:28:00 -0700

    show the discount

--- graham.py
+++ graham.py
@@ -14,14 +14,25 @@ def get_combined_metrics(symbols, years, output_format="json"):
     # Convert "EPS" and "BVPS" columns to numeric, handling non-numeric values as NaN
     combined_df["EPS"] = pd.to_numeric(combined_df["EPS"], errors="coerce")
     combined_df["BVPS"] = pd.to_numeric(combined_df["BVPS"], errors="coerce")
+    # Convert the "closing_price" column to a numeric data type (float)
+    combined_df["closing_price"] = pd.to_numeric(combined_df["closing_price"], errors="coerce")
+
+   
 
     # Calculate the Graham number and add it as a new column, rounded to 2 decimal places
     combined_df["Graham Number"] = combined_df.apply(
         lambda row: round(math.sqrt(22.5 * row["EPS"] * row["BVPS"]), 2), axis=1
     )
+     # Calculate the BG_Perc column
+    combined_df["BG_Perc"] = combined_df.apply(
+        lambda row: round(((row["closing_price"]-row["Graham Number"]) / row["closing_price"]) * 100, 2),
+        axis=1
+    )
+
+
 
     # Select and reorder columns for the final dataframe
-    combined_df = combined_df[["Symbol", "Graham Number", "BVPS", "EPS"]]
+    combined_df = combined_df[["Symbol", "Graham Number", "closing_price", "BG_Perc","BVPS", "EPS"]]
 
     # Return the combined data in the specified output format
     if output_format == "json":
@@ -36,7 +47,7 @@ def get_combined_metrics(symbols, years, output_format="json"):
         return "Invalid output format"
 
 # Example usage:
-symbols = ["TD", "TOU", "CNQ", "ACO.X", "NA"]
+symbols = ["TD", "TOU", "CNQ", "ACO.X", "NA", "ENB", "TRP", "RY", "CM", "BNS"]
 years = 5
 output_format = "csv"
 result = get_combined_metrics(symbols, years, output_format)
