cbc9c63a5c3662223a1b6f689cbd27d14a1f42f3
jmcilroy <jmcilroy@gmail.com>
2024-11-12 07:32:22 -0700

    added some symbols t the divhist file

--- divhist.py
+++ divhist.py
@@ -3,10 +3,7 @@ import datetime
 import pandas as pd
 #This uses yfinance.  I found there are instances in the historical data where it doesn't reflect reality (missing one for TD in 2023)
 def avg_div_grwth(symbols, num_years):
-    # Get the current year
     current_year = datetime.date.today().year
-
-    # Create a list to store the results
     results = []
 
     for symbol in symbols:
@@ -15,33 +12,25 @@ def avg_div_grwth(symbols, num_years):
         stock = yf.Ticker(f"{symbol}.to")
         dividend_data = stock.dividends
         total_dividends = dividend_data.resample('Y').sum()
-        print (total_dividends)
+        #print (total_dividends)
         dividend_growth_rate = total_dividends.pct_change() * 100
         last_years_data = dividend_growth_rate.loc[(dividend_growth_rate.index.year >= current_year - num_years) & (dividend_growth_rate.index.year < current_year)]
         average_growth_rate = round(last_years_data.mean(), 2)
         results.append({"Symbol": ogSymbol, "avg_div_grwth": average_growth_rate, "yrs": num_years})
-
-    # Create a DataFrame from the results
     df = pd.DataFrame(results)
-
-    # Sort the DataFrame in descending order by "Average Growth Rate"
     df = df.sort_values(by="avg_div_grwth", ascending=False).reset_index(drop=True)
 
     return df
 
 def cleanSymbol(input_string):
-    # Check if the string contains a period
     if "." in input_string:
-        # Replace the period with a hyphen
         result = input_string.replace(".", "-")
         return result
     else:
-        # If no period is found, return the original string
         return input_string
 
 if __name__ == "__main__":
-    # Example usage:
-    symbols = ["td"]
+    symbols = ["BMO","TD"]
     num_years = 5
     result_df = avg_div_grwth(symbols, num_years)
     print(result_df)
