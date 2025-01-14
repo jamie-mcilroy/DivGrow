0f84435d4a0bb482ddc8572d0662428ba42f0dda
Jamie McIlroy <jmcilroy@gbefund.com>
2025-01-14 06:53:37 -0700

    removed unneeded files

--- yfinancetest.py
+++
@@ -1,8 +0,0 @@
-from divhist2 import get_dividend_growth
-
-ticker = "TD"  # Example ticker
-years_back = 5  # Example number of years back
-
-# Get the average dividend growth
-ticker, avg_growth, years_used = get_dividend_growth(ticker, years_back)
-print(f"Ticker: {ticker}, Average Growth: {avg_growth}%, Years: {years_used}")
